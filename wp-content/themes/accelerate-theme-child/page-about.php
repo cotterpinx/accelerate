16f1bfb6e6ce701184b307589c6d9e8e4ab7a27f
cotterpinx <cotterpinx@gmail.com>
2016-10-06 19:46:09 -0400

    Contact Us page updated

---
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the Accelerate About page.
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.1
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2 class="page-content"><?php the_title(); ?></h2>
+				<p class="page-content">
+					<?php the_content(); ?>
+				</p>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
